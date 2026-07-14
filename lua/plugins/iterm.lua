a75ad88cf7c2a0b27ab57ee4a1afe00116c09130
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-07-14 11:19:01 +0200

    Set the terminal session title to be CWD

--- lua/plugins/iterm.lua
+++ lua/plugins/iterm.lua
@@ -1,6 +1,17 @@
 return {
-  "izeau/iterm.nvim",
+  "ombratteng/iterm.nvim",
   version = false,
   cond = vim.env.TERM_PROGRAM == "iTerm.app",
   opts = { profile = "Neovim" },
+  config = function(_, opts)
+    local iterm = require("iterm")
+    iterm.setup(opts)
+
+    local function set_title()
+      iterm.set_name(vim.fn.fnamemodify(vim.fn.getcwd(), ":t"))
+    end
+
+    set_title()
+    vim.api.nvim_create_autocmd("DirChanged", { callback = set_title })
+  end,
 }
