076209118b044bec2b7a3715ef292ce71499aa24
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-07-20 11:49:48 +0200

    Always show diagostic source

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.diagnostic.config(vim.tbl_deep_extend("force", vim.diagnostic.config() or {}, {
+  virtual_text = { source = "if_many" },
+  float = { source = true },
+}))
