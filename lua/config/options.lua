7e1d96a05b4af443105e58038b8f5ebedbe970aa
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-07-28 18:28:34 +0200

    Enable SSH clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,35 @@ vim.diagnostic.config(vim.tbl_deep_extend("force", vim.diagnostic.config() or {}
   virtual_text = { source = "if_many" },
   float = { source = true },
 }))
+
+if vim.env.SSH_TTY then
+  vim.opt.clipboard = "unnamedplus"
+
+  local function paste()
+    return { vim.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+  end
+
+  local copy
+  local name
+  if vim.fn.executable("it2copy") == 1 then
+    name = "it2copy"
+    copy = { "bash", "-c", "it2copy > $SSH_TTY" }
+  else
+    name = "OSC 52"
+    local osc52 = require("vim.ui.clipboard.osc52")
+    copy = nil
+    vim.g.clipboard = {
+      name = name,
+      copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("*") },
+      paste = { ["+"] = paste, ["*"] = paste },
+    }
+  end
+
+  if copy then
+    vim.g.clipboard = {
+      name = name,
+      copy = { ["+"] = copy, ["*"] = copy },
+      paste = { ["+"] = paste, ["*"] = paste },
+    }
+  end
+end
