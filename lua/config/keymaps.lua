73837d72b230aaa801b12a2df1068b5040755af2
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-08-07 11:24:50 +0200

    Install oh-my-py plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>ao", function()
+  Snacks.terminal.toggle("omp", {
+    win = {
+      position = "right",
+      width = 0.4,
+    },
+  })
+end, { desc = "Toggle Oh My Pi" })
